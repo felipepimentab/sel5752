7a6b8cc2c5ddb7cd1ef057d9f24a1efc911e0afa
Felipe Pimenta <felipe.pbernardo@gmail.com>
2024-04-16 17:15:28 -0300

    adds flopr to ghdl dir

---
+++ ghdl/flopr.vhd
@@ -0,0 +1,26 @@
+---------------------------------
+-- Título: flopr
+-- Descrição: Flop R
+-- Autor: Felipe Pimenta Bernardo
+-- Data: 16/04/2024
+---------------------------------
+
+entity flopr is
+    port (
+        clk, reset : in bit;
+        d : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+end flopr;
+
+architecture behaviour of flopr is
+    begin
+        process (clk, reset)
+        begin
+            if reset = '1' then
+                q <= (others => '0');
+            elsif (clk'event and clk = '1') then
+                q <= d;
+            end if;
+        end process;
+    end;
